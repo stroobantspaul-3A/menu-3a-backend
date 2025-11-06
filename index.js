bd5737458929852148415721b18fe70e5859ff67
stroobantspaul-3A <stroobantspaul@gmail.com>
2025-11-06 10:02:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // 🔹 URL de tu App Script (cámbiala por la tuya si no es esta)
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwHQUBSI7_7SBXQGyGsgTSAB1NgpHYIVSGy11C9ULNBeyfBBLYrLRK5VtNLQQvD96Fh/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOVjYYtzuwvG_Yape2k-uXRo8UTS2NS67tZQRHOtd-o2E5eD4LF10AcTzBH20ujfT2/exec";
 
 // ✅ Ruta para obtener el menú
 app.get("/menu", async (req, res) => {
